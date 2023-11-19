cb181c00d127d6f0acbbe2a648a7dff1bf66571f
zachary tecson <zactec115@gmail.com>
2023-11-19 22:32:51 +0800

    day 1

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,6 +1,12 @@
-name = None
-age = None
-isMale = None
-department = None
+name = 'John Zachary Mereul Tecson'
+age = 14
+isMale = True
+department = 'Department of Software and Web Development'
 
-print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
+your_name = name
+name_of_department = department
+about_you = 'i hate programming!'
+message = f"Hello, I am {your_name}. I love {name_of_department} and I love being in the Backend even more!\nOne thing about me is that {about_you}."
+
+print(message)
+# print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
