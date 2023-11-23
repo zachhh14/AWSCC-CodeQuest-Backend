6feed1b9de9682f3375e5928d2dac5e1c29da565
zachary tecson <zactec115@gmail.com>
2023-11-23 21:30:06 +0800

    day 25

---
+++ backend/day-25/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+app.config['DB_NAME'] = 'books.db'
+
+def get_db():
+    db = sqlite3.connect(app.config['DB_NAME'])
+    db.row_factory = sqlite3.Row
+    return db
+
+@app.route('/')
+def index():
+    conn = get_db()
+    books = conn.execute("SELECT * FROM books").fetchall()
+    conn.close()
+    return render_template('./index.html', books=books)
+
+if __name__ == '__main__':
+    app.run()
+
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return render_template('./index.html',title='Home', user='Zach')
+
+# if __name__ == '__main__':
+#     app.run()
